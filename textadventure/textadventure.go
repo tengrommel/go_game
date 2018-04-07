363701aca6a561d0c24e8e8edf9089422eeca95a
tengrommel <wwwsousoucom@126.com>
2018-04-07 18:45:21 +0800

    图计算

--- textadventure/textadventure.go
+++ textadventure/textadventure.go
@@ -1,15 +1,30 @@
 package main
 
 type choices struct {
+	cmd        string
 	desription string
-	next *choices
+	nextNode       *storyNode
+	nextChoice       *choices
 }
-
 
 type storyNode struct {
-	text string
-
+	text    string
+	choices *choices
 }
+
+func (node *storyNode)addChoice(cmd string, description string, nextNode *storyNode)  {
+	choice := &choices{cmd, description, nextNode, nil}
+	if node.choices == nil{
+		node.choices = choice
+	} else {
+		currentChoice := node.choices
+		for currentChoice.nextNode!= nil{
+			currentChoice = currentChoice.nextChoice
+		}
+		currentChoice.nextChoice = choice
+	}
+}
+
 
 func main() {
 
